202d3463023ba42187f21ea9424aa89ee510e194
mojila <me.mojila@gmail.com>
2025-08-22 14:34:27 +0700

    feat(login): add id to login button for testing purposes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -127,6 +127,7 @@ export default function LoginPage() {
 
           <div>
             <button
+              id='login-button'
               type="submit"
               disabled={isSubmitting}
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50"
